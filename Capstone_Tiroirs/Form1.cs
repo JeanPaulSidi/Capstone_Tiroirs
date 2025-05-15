08460221ce3142aa573aca71d1f56c1716cd32a9
jeanpaul sidibe <jeanpaul.sidibe@student.centralia.edu>
2025-05-14 23:42:56 -0700

    Add project files.

---
+++ Capstone_Tiroirs/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capstone_Tiroirs
+{
+    public partial class Form1 : Form
+    {
+        int[] num = {3, 1, 2, 6, 3, 9, 20};
+        List<string> list = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Resultats() 
+        {
+            int Counter = 0;
+            foreach (int i in num) 
+            {
+                //Counter++;
+                //list.Add($"Tiroir{Counter}: {i} ");
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Resultats();
+            //label1.Text = string.Join(",  ", list);
+            int counter = 0;
+            label1.Text = string.Empty;
+            foreach (int i in num) 
+            {
+                counter++;
+                label1.Text += ($"Tiroir{counter} : {i} boules,  ");
+                if (counter % 5 == 0)
+                    label1.Text += Environment.NewLine;
+            }
+        }
+    }
+}
